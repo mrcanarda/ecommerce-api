b65ff574ac9e4d8e128d326446d79ffad4fb13d5
Can Arda <canarda.dev@gmail.com>
2026-06-10 21:01:22 +0300

    Generic Repository Pattern

---
+++ ECommerce.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using ECommerce.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ ECommerce.Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic; 
+
+namespace ECommerce.Data.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id); 
+        Task<IEnumerable<T>> GetAllAsync(); 
+        Task AddAsync(T entity);
+        void Update(T entity); 
+        void Delete(T entity); 
+    }
+}
